f46fc190e0a8719e445f4ec83118236e9d985c92
Aakanksha-Pandey25 <aakanksha.pandey@gammaedge.io>
2025-06-26 12:33:38 +0530

    new commit

---
+++ Python/action1.py
@@ -0,0 +1,19 @@
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+import time
+
+driver=webdriver.Chrome()
+driver.get(" https://demoqa.com/buttons")
+time.sleep(2)
+driver.maximize_window()
+action=ActionChains(driver)
+
+double_click_button=driver.find_element(By.ID,"doubleClickBtn")
+action.context_click(double_click_button).perform()
+right_click_btn = driver.find_element(By.ID, "rightClickBtn")
+action.context_click(right_click_btn).perform()
+
+
+time.sleep(10)
